9c085ce1f40acd0c51d75f1607d6e0340ed9b6eb
Özgür Tokay <ozytky@gmail.com>
2024-02-04 21:19:21 +0300

    Hashing password

--- ws/src/main/java/com/rumorify/ws/service/UserService.java
+++ ws/src/main/java/com/rumorify/ws/service/UserService.java
@@ -1,6 +1,9 @@
 
 package com.rumorify.ws.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.rumorify.ws.model.User;
@@ -13,17 +16,20 @@ import lombok.AllArgsConstructor;
  *
  */
 @Service
-@AllArgsConstructor
 public class UserService {
 
-	private final UserRepository userRepository;
-	
+	@Autowired
+	private  UserRepository userRepository;
+
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 	public User findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-	
+		return userRepository.findByUsername(username);
+	}
+
 	public User save(User user) {
-        return userRepository.save(user);
-    }
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		return userRepository.save(user);
+	}
 
 }

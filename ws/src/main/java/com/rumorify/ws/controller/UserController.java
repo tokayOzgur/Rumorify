1d4654ac536e00384a4f2b0a745d1fd753f8d9a4
Özgür Tokay <ozytky@gmail.com>
2024-02-04 17:31:27 +0300

    DB Configuration and vite.conf.js Proxy

--- ws/src/main/java/com/rumorify/ws/controller/UserController.java
+++ ws/src/main/java/com/rumorify/ws/controller/UserController.java
@@ -1,28 +1,25 @@
 
 package com.rumorify.ws.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.rumorify.ws.model.User;
 
 /**
  * @author tokay
  *
  */
 @RestController
-@CrossOrigin("http://localhost:5173/")
 @RequestMapping("/api/v1/users")
 public class UserController {
-	
-	@PostMapping(value="/addUser")
-	public SomeEnityData createUser(@RequestBody SomeEnityData entity) {
-		//TODO: process POST request
-		
-		return entity;
+
+	@PostMapping(value = "/addUser")
+	public void createUser(@RequestBody User user) {
+		System.out.println(user);
+
 	}
-	
 
 }

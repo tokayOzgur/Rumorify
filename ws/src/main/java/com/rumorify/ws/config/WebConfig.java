d4ecae3e7bc2665894374df2d35c75f8362329db
Özgür Tokay <ozytky@gmail.com>
2024-02-08 21:47:39 +0300

    Add @NonNull annotation to addCorsMappings method in WebConfig.java

--- ws/src/main/java/com/rumorify/ws/config/WebConfig.java
+++ ws/src/main/java/com/rumorify/ws/config/WebConfig.java
@@ -1,13 +1,14 @@
 package com.rumorify.ws.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")

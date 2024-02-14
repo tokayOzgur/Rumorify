60203a9f2d9f7951e378b9247dcc95fdb677a017
Özgür Tokay <ozytky@gmail.com>
2024-02-14 22:34:32 +0300

    Add Navbar component and import Bootstrap JS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS
 import { SignUp } from "./pages/SignUp";
 import { Provider } from "react-redux";
 import React from "react";
 import { store } from "./redux/store";
+import { Navbar } from "./shared/components/Navbar";
 
 function App() {
   return (
     <React.StrictMode>
       <Provider store={store}>
         <div className="cotanier-fluid">
+          <Navbar />
           <SignUp />
         </div>
       </Provider>

580c61c9f9b824bb798864a94ca541c39d6a21ae
Özgür Tokay <ozytky@gmail.com>
2024-02-14 22:33:55 +0300

    Update i18n initialization and add language options

--- frontend/src/locales/index.js
+++ frontend/src/locales/index.js
@@ -3,16 +3,22 @@ import { initReactI18next } from "react-i18next";
 import trJson from "./translations/tr.json";
 import enJson from "./translations/en.json";
 
-i18n.use(initReactI18next).init({
+const initialLanguage =
+  localStorage.getItem("lang") || navigator.language || "en";
+
+export const i18nInstance = i18n.use(initReactI18next);
+
+i18nInstance.init({
   resources: {
     en: {
-      translations: enJson,
+      translation: enJson,
     },
     tr: {
-      translations: trJson,
+      translation: trJson,
     },
   },
-  fallbackLng: "en",
+  fallbackLng: initialLanguage,
+
   interpolation: {
     escapeValue: false,
   },

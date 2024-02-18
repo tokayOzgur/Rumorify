d65eef5d3710644663654ab0c636a1c9453cd235
Özgür Tokay <ozytky@gmail.com>
2024-02-18 19:01:41 +0300

    Add .env.example file and update dependencies for dotenv-java

--- ws/src/main/java/com/rumorify/ws/service/impl/EmailServiceImp.java
+++ ws/src/main/java/com/rumorify/ws/service/impl/EmailServiceImp.java
@@ -2,38 +2,48 @@ package com.rumorify.ws.service.impl;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
+import com.rumorify.ws.config.RumorifyProperties;
 import com.rumorify.ws.service.EmailService;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class EmailServiceImp implements EmailService {
-    // TODO use gmail smtp server
 
-    @Override
-    public void sendActivationEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("testmail@gmail.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        getJavaMailSender().send(message);
-    }
+    private JavaMailSenderImpl mailSender;
+    Dotenv dotenv = Dotenv.load();
 
-    @Override
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.ethereal.email");
-        mailSender.setPort(587);
-        mailSender.setUsername("veronica.mcdermott@ethereal.email");
-        mailSender.setPassword("fvwWtsa8QXH3TRr7z1");
+    @Autowired
+    private RumorifyProperties rumorifyProp;
+
+    @PostConstruct
+    public void initialize() {
+        this.mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(rumorifyProp.getEmail().host());
+        mailSender.setPort(rumorifyProp.getEmail().port());
+        mailSender.setUsername(dotenv.get("RUMORIFY_EMAIL_USERNAME"));
+        mailSender.setPassword(dotenv.get("RUMORIFY_EMAIL_PASSWORD"));
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.starttls.enable", "true");
-        return mailSender;
+    }
+
+    @Override
+    public void sendActivationEmail(String email, String activationToken) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(rumorifyProp.getEmail().from());
+        mailMessage.setTo(email);
+        mailMessage.setSubject("Rumorify Account Activation");
+        mailMessage.setText("To activate your account, please click the following link: "
+                + rumorifyProp.getClient().host() + "/activate?token=" + activationToken);
+
+        this.mailSender.send(mailMessage);
     }
 
 }

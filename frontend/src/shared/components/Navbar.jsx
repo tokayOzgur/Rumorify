85820089c22643045c0e9f3b4f9ee06e16553a71
Özgür Tokay <ozytky@gmail.com>
2024-02-21 23:02:55 +0300

    Add translations and update Navbar component

--- frontend/src/shared/components/Navbar.jsx
+++ frontend/src/shared/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import { LanguageSelector } from "./LanguageSelector";
+import logo from "../../assets/a.png";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export const Navbar = () => {
+  const { t } = useTranslation();
+
   return (
-    <div className="m-2 shadow-lg">
+    <div className="m-2 shadow-sm">
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
+            <img
+              src={logo}
+              alt="rumorify_logo"
+              width={60}
+              className="rounded mx-2"
+            />
             Rumorify
           </a>
           <button
@@ -21,16 +32,6 @@ export const Navbar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
@@ -39,7 +40,7 @@ export const Navbar = () => {
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Dropdown
+                  {t("preferences")}
                 </a>
                 <ul className="dropdown-menu">
                   <li>
@@ -48,17 +49,22 @@ export const Navbar = () => {
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
                     <hr className="dropdown-divider" />
                   </li>
                   <LanguageSelector />
                 </ul>
               </li>
+              <li>
+                <Link className="nav-link" to={"/aktivation"}>
+                  {t("activation")}
+                </Link>
+              </li>
             </ul>
+            <div className="d-flex mx-2">
+              <Link className="nav-link" to="/signup">
+                {t("signUp")}
+              </Link>
+            </div>
           </div>
         </div>
       </nav>

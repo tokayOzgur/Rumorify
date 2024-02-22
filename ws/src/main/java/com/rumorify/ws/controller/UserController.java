8f910d8f798be299015e1cb45fba3f9ffb4462c8
Özgür Tokay <ozytky@gmail.com>
2024-02-22 22:39:14 +0300

    Add PATCH endpoint to activate user

--- ws/src/main/java/com/rumorify/ws/controller/UserController.java
+++ ws/src/main/java/com/rumorify/ws/controller/UserController.java
@@ -2,6 +2,8 @@
 package com.rumorify.ws.controller;
 
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,13 @@ public class UserController {
 	public GenericMessage createUser(@Valid @RequestBody CreateUserRequest userRequest) {
 		userService.save(userRequest);
 		return new GenericMessage(Messages.getMessageForLocale("rumorify.create.user.success.message",
+				LocaleContextHolder.getLocale()));
+	}
+
+	@PatchMapping(value = "/{token}/active")
+	public GenericMessage activateUser(@PathVariable String token) {
+		userService.activeUser(token);
+		return new GenericMessage(Messages.getMessageForLocale("rumorify.activate.user.success.message",
 				LocaleContextHolder.getLocale()));
 	}
 

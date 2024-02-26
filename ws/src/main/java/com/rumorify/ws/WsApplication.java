1dc0e3381a933b3ba4e2de6cc18b363f3540b823
Özgür Tokay <ozytky@gmail.com>
2024-02-26 22:11:19 +0300

    Add user creation on application startup

--- ws/src/main/java/com/rumorify/ws/WsApplication.java
+++ ws/src/main/java/com/rumorify/ws/WsApplication.java
@@ -1,8 +1,16 @@
 package com.rumorify.ws;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.rumorify.ws.model.User;
+import com.rumorify.ws.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WsApplication {
@@ -11,4 +19,19 @@ public class WsApplication {
 		SpringApplication.run(WsApplication.class, args);
 	}
 
+	@Bean
+	@Profile("dev")
+	CommandLineRunner userCreator(UserRepository userRepository) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return args -> {
+			for (int i = 0; i < 25; i++) {
+				User user = new User();
+				user.setUsername("User " + i);
+				user.setEmail("user" + i + "@gmail.com");
+				user.setPassword(passwordEncoder.encode("P4assword!"));
+				user.setActive(true);
+				userRepository.save(user);
+			}
+		};
+	}
 }

4a234eac2ead8a0d8f7ddf0edac23a1b87802839
Özgür Tokay <ozytky@gmail.com>
2024-03-02 22:51:57 +0300

    Add new fields to GetAllActiveUsersResponse class

--- ws/src/main/java/com/rumorify/ws/dto/responses/GetAllActiveUsersResponse.java
+++ ws/src/main/java/com/rumorify/ws/dto/responses/GetAllActiveUsersResponse.java
@@ -6,5 +6,10 @@ import lombok.Data;
 public class GetAllActiveUsersResponse {
     private int id;
     private String username;
+    private String firstName;
+    private String lastName;
     private String email;
+    private String image;
+    private String profileDescription;
+
 }

326e74eebdc93eff7b24d7514d3db1d67232f297
Özgür Tokay <ozytky@gmail.com>
2024-03-06 22:41:11 +0300

    Add Spinner component to Button.jsx

--- frontend/src/shared/components/Button.jsx
+++ frontend/src/shared/components/Button.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Spinner } from "./Spinner";
 
-export const Button = ({btnClass, apiProgress, disabled, children }) => {
+export const Button = ({ btnClass, apiProgress, disabled, children }) => {
   return (
     <button className="btn btn-primary" disabled={apiProgress || disabled}>
       {apiProgress && <Spinner size="sm" />}

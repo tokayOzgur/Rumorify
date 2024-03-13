9320341482807118649cbb897af6ebcbb8cd3df2
Özgür Tokay <ozytky@gmail.com>
2024-03-13 23:15:15 +0300

    Refactor App component import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,18 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from "react";
 import { Provider } from "react-redux";
 import "./App.css";
-import { store } from "./redux/store";
+import { store } from "./app/store";
 import { Navbar } from "./shared/components/Navbar";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <React.StrictMode>
-      <Provider store={store}>
-        <div className="cotanier-fluid">
-          <Navbar />
-          <Outlet />
-        </div>
-      </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+      <div className="cotanier-fluid">
+        <Navbar />
+        <Outlet />
+      </div>
+    </Provider>
   );
 }
 

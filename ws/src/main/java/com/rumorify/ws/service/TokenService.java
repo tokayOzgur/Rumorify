3ebacf99a64ce866cda312aa2cc6597ec2f4f7e4
Özgür Tokay <ozytky@gmail.com>
2024-03-16 19:05:43 +0300

    Refactor TokenService interface and TokenServiceImpl class

--- ws/src/main/java/com/rumorify/ws/service/TokenService.java
+++ ws/src/main/java/com/rumorify/ws/service/TokenService.java
@@ -2,11 +2,10 @@ package com.rumorify.ws.service;
 
 import com.rumorify.ws.dto.requests.CredentialsRequest;
 import com.rumorify.ws.dto.responses.GetUserByEmailResponse;
-import com.rumorify.ws.model.User;
 import com.rumorify.ws.token.Token;
 
 public interface TokenService {
     public Token generateToken(GetUserByEmailResponse user, CredentialsRequest credentials);
 
-    public User verifyToken(String authorizationHeader);
+    public int verifyToken(String authorizationHeader);
 }
--- ws/src/main/java/com/rumorify/ws/service/impl/TokenServiceImpl.java
+++ ws/src/main/java/com/rumorify/ws/service/impl/TokenServiceImpl.java
@@ -2,16 +2,24 @@ package com.rumorify.ws.service.impl;
 
 import java.util.Base64;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.rumorify.ws.dto.requests.CredentialsRequest;
 import com.rumorify.ws.dto.responses.GetUserByEmailResponse;
-import com.rumorify.ws.model.User;
 import com.rumorify.ws.service.TokenService;
+import com.rumorify.ws.service.UserService;
 import com.rumorify.ws.token.Token;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 @Service
+@Data
+@AllArgsConstructor
 public class TokenServiceImpl implements TokenService {
+    private final UserService tokenService;
 
     @Override
     public Token generateToken(GetUserByEmailResponse user, CredentialsRequest credentials) {
@@ -21,9 +29,18 @@ public class TokenServiceImpl implements TokenService {
     }
 
     @Override
-    public User verifyToken(String authorizationHeader) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'verifyToken'");
+    public int verifyToken(String authorizationHeader) {
+        if (authorizationHeader == null)
+            return 0;
+        String base64encoded = authorizationHeader.split("Basic ")[1];
+        String decoded = new String(Base64.getDecoder().decode(base64encoded));
+        CredentialsRequest credentials = new CredentialsRequest(decoded.split(":")[0], decoded.split(":")[1]);
+        GetUserByEmailResponse inDB = tokenService.findByEmail(credentials.email());
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        if (inDB != null && passwordEncoder.matches(credentials.password(), inDB.getPassword()))
+            return inDB.getId();
+        return 0;
     }
 
 }

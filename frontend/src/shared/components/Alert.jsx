195e1fb5b6d1d20d851a906e7a17bfadfe0922ac
Özgür Tokay <ozytky@gmail.com>
2024-03-17 22:18:31 +0300

    Refactor Alert component to use arrow function syntax

--- frontend/src/shared/components/Alert.jsx
+++ frontend/src/shared/components/Alert.jsx
@@ -1,11 +1,8 @@
-import PropTypes from "prop-types";
-
 export const Alert = (props) => {
   const { children, styleType } = props;
-  return <div className={`alert alert-${styleType || "info"}`}>{children}</div>;
-};
-
-Alert.propTypes = {
-  children: PropTypes.node.isRequired,
-  styleType: PropTypes.string,
+  return (
+    <div className={`alert alert-${styleType || "info"}`}>
+      {children}
+    </div>
+  );
 };

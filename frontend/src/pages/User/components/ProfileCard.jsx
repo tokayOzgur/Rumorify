86a98e7822f28a993730b0c3012c38781d3c0d02
Özgür Tokay <ozytky@gmail.com>
2024-03-17 21:19:10 +0300

    Add edit functionality to ProfileCard component

--- frontend/src/pages/User/components/ProfileCard.jsx
+++ frontend/src/pages/User/components/ProfileCard.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import defaultProfileImage from "@/assets/defUser.png";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
+import { Button } from "@/shared/components/Button";
+import { Input } from "@/shared/components/Input";
+import { updateUser } from "@/api/userApi";
+import { Alert } from "@/shared/components/Alert";
 
 export const ProfileCard = ({ user }) => {
   const authState = useSelector((store) => store.auth);
   const { t } = useTranslation();
+  const [apiProgress, setApiProgress] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const isEditable = !editMode && authState.id === user.id;
+  const [newUsername, setNewUsername] = useState(user.username);
+  const [newProfileDescription, setNewProfileDescription] = useState(
+    user.profileDescription
+  );
+  const [newFirstName, setNewFirstName] = useState(user.firstName);
+  const [newLastName, setNewLastName] = useState(user.lastName);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleUpdate = () => {
+    setApiProgress(true);
+    setErrorMessage("");
+    setSuccessMessage("");
+    user.username = newUsername;
+    user.profileDescription = newProfileDescription;
+    user.firstName = newFirstName;
+    user.lastName = newLastName;
+    updateUser(user.id, user)
+      .then((response) => {
+        console.log("response::", response);
+        setSuccessMessage("User updated successfully");
+      })
+      .catch((error) => {
+        console.log("error::", error);
+        setErrorMessage(error.response.data.validationError);
+      })
+      .finally(() => {
+        setApiProgress(false);
+        setEditMode(false);
+      });
+  };
 
   return (
     <div className="card m-5">
+      {errorMessage && <Alert styleType="danger" children={errorMessage} />}
+      {successMessage && (
+        <Alert styleType="success" children={successMessage} />
+      )}
       <div className="row no-gutters">
         <div className="col-md-4">
           <img
@@ -19,20 +61,84 @@ export const ProfileCard = ({ user }) => {
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <button className="btn btn-info float-end">{t("edit")}</button>
-            <h5 className="card-title">{user.username}</h5>
-            <p className="card-text">{user.description}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                {user.firstName + " " + user.lastName}
-              </small>
-            </p>
-            <p className="card-text">
-              <strong>{user.email}</strong>
-            </p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
+            {isEditable && (
+              <Button
+                btnClass={"info float-end mb-1"}
+                onClick={() => {
+                  setEditMode(true);
+                }}
+              >
+                {t("edit")}
+              </Button>
+            )}
+
+            {editMode ? (
+              <>
+                <Input
+                  label={t("username")}
+                  type={"text"}
+                  id={user.username}
+                  defaultValue={user.username}
+                  onChange={(e) => {
+                    setNewUsername(e.target.value);
+                  }}
+                />
+                <Input
+                  label={t("description")}
+                  type={"text"}
+                  id={user.profileDescription}
+                  defaultValue={user.profileDescription}
+                  onChange={(e) => {
+                    setNewProfileDescription(e.target.value);
+                  }}
+                />
+                <Input
+                  label={t("firstName")}
+                  type={"text"}
+                  id={user.firstName}
+                  defaultValue={user.firstName}
+                  onChange={(e) => {
+                    setNewFirstName(e.target.value);
+                  }}
+                />
+                <Input
+                  label={t("lastName")}
+                  type={"text"}
+                  id={user.lastName}
+                  defaultValue={user.lastName}
+                  onChange={(e) => {
+                    setNewLastName(e.target.value);
+                  }}
+                />
+                <Button
+                  btnClass={"primary float-end my-2"}
+                  apiProgress={apiProgress}
+                  children={t("save")}
+                  onClick={handleUpdate}
+                />
+                <Button
+                  btnClass={"outline-secondary mx-2 my-2 float-end"}
+                  onClick={() => {
+                    setEditMode(false);
+                  }}
+                  children={t("cancel")}
+                />
+              </>
+            ) : (
+              <>
+                <span className="badge text-bg-secondary mb-3">3m ago</span>
+                <h5 className="card-title">{user.username}</h5>
+                <p className="card-text">{user.profileDescription}</p>
+                <p className="card-text">
+                  <small className="text-muted">
+                    {user.firstName + " " + user.lastName}
+                  </small>
+                </p>
+                <p className="card-text">
+                  <strong>{user.email}</strong>
+                </p>
+              </>
+            )}
           </div>
         </div>
       </div>

e8ca95e37aca26e4cd96ce644e81575013639fe5
Özgür Tokay <ozytky@gmail.com>
2024-03-17 21:19:06 +0300

    Refactor Button and Input components

--- frontend/src/shared/components/Button.jsx
+++ frontend/src/shared/components/Button.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Spinner } from "./Spinner";
 
-export const Button = ({ btnClass, apiProgress, disabled, children }) => {
+export const Button = ({
+  btnClass = "primary",
+  apiProgress,
+  disabled,
+  children,
+  onClick,
+}) => {
   return (
-    <button className="btn btn-primary" disabled={apiProgress || disabled}>
+    <button
+      className={`btn btn-${btnClass}`}
+      disabled={apiProgress || disabled}
+      onClick={onClick}
+    >
       {apiProgress && <Spinner size="sm" />}
       {children}
     </button>
--- frontend/src/shared/components/Input.jsx
+++ frontend/src/shared/components/Input.jsx
@@ -1,14 +1,15 @@
 export const Input = (prop) => {
-  const { id, label, error, onChange, type } = prop;
+  const { id, label, error, onChange, type, defaultValue } = prop;
   return (
-    <div className="">
-      <label htmlFor={id}>{label}</label>
+    <div>
+      {label && <label htmlFor={id}>{label}</label>}
       <input
         className={error ? "form-control is-invalid" : "form-control mb-3"}
         type={type}
         id={id}
         name={id}
         onChange={onChange}
+        defaultValue={defaultValue}
       />
       <div className="invalid-feedback">{error}</div>
     </div>

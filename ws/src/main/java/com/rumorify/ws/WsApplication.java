8d3d57033eeb32280a61f96694fe71bc16f57edc
Özgür Tokay <ozytky@gmail.com>
2024-03-28 22:07:37 +0300

    Fix user creation loop and add new user

--- ws/src/main/java/com/rumorify/ws/WsApplication.java
+++ ws/src/main/java/com/rumorify/ws/WsApplication.java
@@ -21,18 +21,28 @@ public class WsApplication {
 	@Profile("dev")
 	CommandLineRunner userCreator(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		return args -> {
-			for (int i = 0; i < 25; i++) {
+			for (int i = 1; i < 25; i++) {
 				User user = new User();
 				user.setUsername("User " + i);
 				user.setFirstName("firstName " + i);
 				user.setLastName("lastName " + i);
 				user.setProfileDescription("Hi! I'm user " + i);
 				user.setEmail("user" + i + "@gmail.com");
-				user.setPassword(passwordEncoder.encode("P4assword!"));
+				user.setPassword(passwordEncoder.encode("P4ssword!"));
 				user.setActive(true);
 				user.setImage("https://i.pravatar.cc/600?img=" + i);
 				userRepository.save(user);
 			}
+			User user = new User();
+			user.setUsername("User " + 26);
+			user.setFirstName("firstName " + 26);
+			user.setLastName("lastName " + 26);
+			user.setProfileDescription("Hi! I'm user " + 26);
+			user.setEmail("user" + 26 + "@gmail.com");
+			user.setPassword(passwordEncoder.encode("P4ssword!"));
+			user.setActive(false);
+			user.setImage("https://i.pravatar.cc/600?img=" + 26);
+			userRepository.save(user);
 		};
 	}
 }

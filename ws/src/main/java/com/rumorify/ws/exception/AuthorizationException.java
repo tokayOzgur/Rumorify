6df48151f3c28d011252bd70d648de87a472b2fb
Özgür Tokay <ozytky@gmail.com>
2024-03-28 22:08:37 +0300

    Remove AuthorizationException class

--- ws/src/main/java/com/rumorify/ws/exception/AuthorizationException.java
+++
@@ -1,8 +0,0 @@
-package com.rumorify.ws.exception;
-
-public class AuthorizationException extends RuntimeException {
-    // TODO 1: set message to "Authorization failed"
-    public AuthorizationException(String message) {
-        super(message);
-    }
-}

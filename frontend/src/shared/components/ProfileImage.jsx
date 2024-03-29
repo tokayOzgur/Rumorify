0a941feff96eb91ff700687ec2816b1c261bc7ee
Özgür Tokay <ozytky@gmail.com>
2024-03-29 13:23:00 +0300

    Refactor ProfileImage component to include default profile image

--- frontend/src/shared/components/ProfileImage.jsx
+++ frontend/src/shared/components/ProfileImage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import defaultProfileImage from "@/assets/defUser.png";
 
-export const ProfileImage = ({ width, className, alt, src }) => {
-  return <img src={src} alt={alt} width={width} className={className} />;
+export const ProfileImage = ({ width, className, alt, src, height, image }) => {
+  return (
+    <img
+      src={src || image || defaultProfileImage}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+    />
+  );
 };

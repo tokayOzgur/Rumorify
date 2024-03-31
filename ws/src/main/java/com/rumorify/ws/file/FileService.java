b09fdb6ae84ab8e3a355b0c17364a28a63b4c83e
Özgür Tokay <ozytky@gmail.com>
2024-03-31 19:57:53 +0300

    Add FileService functionality for saving and deleting files

--- ws/src/main/java/com/rumorify/ws/file/FileService.java
+++ ws/src/main/java/com/rumorify/ws/file/FileService.java
@@ -1,28 +1,64 @@
 package com.rumorify.ws.file;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.UUID;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.rumorify.ws.config.RumorifyProperties;
 
 @Service
 public class FileService {
+    @Autowired
+    private RumorifyProperties props;
+    private Tika tika = new Tika();
 
     public String saveBase4StringAsFile(String image) {
-        String fileName = UUID.randomUUID().toString();
-        File file = new File(fileName);
-        try (OutputStream os = new FileOutputStream(file)) {
-            byte[] base64decoded = Base64.getDecoder().decode(image.split(",")[1]);
-            os.write(base64decoded);
+        String fileName = UUID.randomUUID().toString() + "." + getFileType(image).split("/")[1];
+        Path path = getFilePath(fileName);
+        try {
+            Files.createDirectories(path.getParent());
+            OutputStream os = new FileOutputStream(path.toFile());
+            os.write(decodedImage(image));
+            os.close();
             return fileName;
         } catch (IOException e) {
+            // TODO: handle exception
             e.printStackTrace();
         }
         return null;
+    }
+
+    public String getFileType(String value) {
+        return tika.detect(decodedImage(value));
+    }
+
+    private byte[] decodedImage(String image) {
+        return Base64.getDecoder().decode(image.split(",")[1]);
+    }
+
+    public void deleteFile(String image) {
+        if (image == null)
+            return;
+        Path path = getFilePath(image);
+        try {
+            Files.deleteIfExists(path);
+        } catch (IOException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    private Path getFilePath(String fileName) {
+        return Paths.get(props.getStorage().getRoot(), props.getStorage().getProfile(), fileName);
     }
 
 }

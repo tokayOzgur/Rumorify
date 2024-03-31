5f059edeb1142e56fe1a79efc87757cb48eebf87
Özgür Tokay <ozytky@gmail.com>
2024-03-31 19:55:37 +0300

    Refactor ProfileImage component to handle temporary source images

--- frontend/src/shared/components/ProfileImage.jsx
+++ frontend/src/shared/components/ProfileImage.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import defaultProfileImage from "@/assets/defUser.png";
 
-export const ProfileImage = ({ width, className, alt, src, height, image }) => {
+export const ProfileImage = ({
+  width,
+  height,
+  className,
+  alt,
+  src,
+  tempSrc,
+}) => {
+  const profileImage = src ? `/assets/profiles/${src}` : defaultProfileImage;
+  console.log("profileImage", profileImage);
   return (
     <img
-      src={src || image || defaultProfileImage}
+      src={tempSrc || profileImage}
       alt={alt}
       width={width}
       height={height}
       className={className}
+      onError={(e) => {
+        e.target.src = defaultProfileImage;
+      }}
     />
   );
 };

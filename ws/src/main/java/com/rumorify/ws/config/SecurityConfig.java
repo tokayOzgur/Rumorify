6b0dc8a52d4c69d59f60b301cba6fbab4b071d9a
Özgür Tokay <ozytky@gmail.com>
2024-04-07 14:31:22 +0300

    Add TokenFilter to SecurityConfig for authentication

--- ws/src/main/java/com/rumorify/ws/config/SecurityConfig.java
+++ ws/src/main/java/com/rumorify/ws/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.rumorify.ws.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,12 +10,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.rumorify.ws.security.TokenFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    TokenFilter tokenFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,6 +32,9 @@ public class SecurityConfig {
         http.httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(new AuthEntryPoint()));
         http.csrf(crsf -> crsf.disable());
         http.headers(headers -> headers.disable());
+
+        http.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 

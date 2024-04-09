d13ee399dda1d02babe64ae5aee1df9836f52b9a
Özgür Tokay <ozytky@gmail.com>
2024-04-09 16:05:29 +0300

    Refactor authSlice to remove setToken calls and update loginSuccess and logoutSuccess reducers

--- frontend/src/features/auth/authSlice.jsx
+++ frontend/src/features/auth/authSlice.jsx
@@ -1,4 +1,4 @@
-import { setToken } from "@/api/axiosInstance";
+// import { setToken } from "@/api/axiosInstance";
 import { loadAuthState } from "@/shared/state/storage";
 import { createSlice } from "@reduxjs/toolkit";
 
@@ -7,14 +7,12 @@ export const authSlice = createSlice({
   initialState: loadAuthState(),
   reducers: {
     loginSuccess: (state, action) => {
-      setToken(action.payload.token);
       state.id = action.payload.user.id;
       state.username = action.payload.user.username;
       state.email = action.payload.user.email;
       state.image = action.payload.user.image;
     },
     logoutSuccess: (state, action) => {
-      setToken(null);
       state.id = 0;
       delete state.username;
       delete state.email;

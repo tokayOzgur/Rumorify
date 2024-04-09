20bfcf27234111a1209cfe1ce8ce9064d581cdf0
Özgür Tokay <ozytky@gmail.com>
2024-04-09 16:04:11 +0300

    Add deleteAllByUserId method to TokenRepository.java

--- ws/src/main/java/com/rumorify/ws/repository/TokenRepository.java
+++ ws/src/main/java/com/rumorify/ws/repository/TokenRepository.java
@@ -1,9 +1,16 @@
 package com.rumorify.ws.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.rumorify.ws.model.Token;
 
-public interface TokenRepository extends JpaRepository<Token, String> {
+import jakarta.transaction.Transactional;
 
+public interface TokenRepository extends JpaRepository<Token, String> {
+    @Modifying
+    @Transactional
+    @Query("delete from Token t where t.user.id =:userId")
+    public void deleteAllByUserId(int userId);
 }

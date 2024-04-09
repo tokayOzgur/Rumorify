db6bc3d356c9d9e138a51bb64af4ec4043a323b9
Özgür Tokay <ozytky@gmail.com>
2024-04-09 16:03:14 +0300

    Refactor AuthController to add logout functionality and handle authentication with cookies

--- ws/src/main/java/com/rumorify/ws/controller/AuthController.java
+++ ws/src/main/java/com/rumorify/ws/controller/AuthController.java
@@ -1,5 +1,9 @@
 package com.rumorify.ws.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,18 +27,32 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/v1/auth")
 @AllArgsConstructor
 public class AuthController {
+    private final AuthService authService;
 
-    final AuthService authService;
-
+    // TODO: projeden Authorization ve localStorage çıkarılacak
     @PostMapping
-    public AuthResponse handleAuthentication(@Valid @RequestBody CredentialsRequest credentials) {
-        return authService.authenticate(credentials);
+    public ResponseEntity<AuthResponse> handleAuthentication(@Valid @RequestBody CredentialsRequest credentials) {
+        AuthResponse authResponse = authService.authenticate(credentials);
+        ResponseCookie cookie = ResponseCookie.from("rumor-token", authResponse.getToken().getToken())
+                .path("/").sameSite("None").secure(true).httpOnly(true).build();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(authResponse);
     }
 
     @DeleteMapping
-    GenericMessage logout(@RequestHeader(name = "Authorization", required = false) String authorizationHeader) {
-        authService.logout(authorizationHeader);
-        return new GenericMessage("User logged out successfully!");
+    public ResponseEntity<GenericMessage> logout(
+            @RequestHeader(name = "Authorization", required = false) String authorizationHeader,
+            @CookieValue(name = "rumor-token", required = false) String cookieValue) {
+
+        System.err.println("********************" + cookieValue);
+        var tokenWithPrefix = authorizationHeader;
+        if (cookieValue != null && !cookieValue.isEmpty())
+            tokenWithPrefix = "AnyPrefix " + cookieValue;
+
+        ResponseCookie cookie = ResponseCookie.from("rumor-token", "")
+                .path("/").httpOnly(true).maxAge(0).build();
+        authService.logout(tokenWithPrefix);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new GenericMessage("User logged out successfully!"));
     }
 
 }

f66a679449cdfff11acfd9038158e534a929ab1a
Özgür Tokay <ozytky@gmail.com>
2024-04-12 23:18:36 +0300

    Refactor App.jsx to add Footer component and improve code structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { fetchCurrentUser } from "@/features/auth/authSlice";
 import { Navbar } from "@/shared/components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@/app.css";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { Footer } from "./shared/components/Footer";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,9 +16,14 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="cotanier-fluid">
-      <Navbar />
-      <Outlet />
+    <div id="main-container">
+      <div className="cotanier-fluid">
+        <Navbar />
+        <div id="content">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }

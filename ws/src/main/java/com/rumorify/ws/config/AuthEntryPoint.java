dadf89d7b7dc2909463d0dd4d9073ba1ec8c29af
Özgür Tokay <ozytky@gmail.com>
2024-04-13 23:03:40 +0300

    Update authentication error message and add unauthorized translation

--- ws/src/main/java/com/rumorify/ws/config/AuthEntryPoint.java
+++ ws/src/main/java/com/rumorify/ws/config/AuthEntryPoint.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.rumorify.ws.shared.Messages;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,7 +24,15 @@ public class AuthEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
-        resolver.resolveException(request, response, null, authException);
+        try {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(Messages.getMessageForLocale("rumorify.authentication.error.message",
+                    LocaleContextHolder.getLocale()));
+            response.getWriter().flush();
+            response.getWriter().close();
+        } catch (Exception e) {
+            resolver.resolveException(request, response, null, authException);
+        }
     }
 
 }

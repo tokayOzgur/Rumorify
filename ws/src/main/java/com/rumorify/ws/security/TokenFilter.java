3f8ba8ec81ac0313ee39d57d1e46ee6c506ad712
Özgür Tokay <ozytky@gmail.com>
2024-04-14 14:59:46 +0300

    Refactor TokenFilter to handle disabled users in doFilterInternal method

--- ws/src/main/java/com/rumorify/ws/security/TokenFilter.java
+++ ws/src/main/java/com/rumorify/ws/security/TokenFilter.java
@@ -2,11 +2,14 @@ package com.rumorify.ws.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import com.rumorify.ws.config.CurrentUser;
 import com.rumorify.ws.model.User;
@@ -16,30 +19,30 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
 
 @Component
-@AllArgsConstructor
 public class TokenFilter extends OncePerRequestFilter {
-
-    private final TokenService tokenService;
+    private HandlerExceptionResolver exceptionResolver;
+    @Autowired
+    private TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String tokenWithPrefix = getTokenWithPrefix(request);
         if (tokenWithPrefix != null) {
-            int userId = tokenService.verifyToken(tokenWithPrefix);
-            if (userId != 0) {
-                // TODO: aktif userlar için kontrol ekle
-                System.out.println("User with id " + userId + " is authenticated");
-                CurrentUser currentUser = new CurrentUser(User.builder().id(userId).build());
+            User user = tokenService.verifyToken(tokenWithPrefix);
+            if (user != null) {
+                if (!user.isActive() || user.isDeleted()) {
+                    exceptionResolver.resolveException(request, response, null,
+                            new DisabledException("User is disabled!"));
+                    return;
+                }
+                CurrentUser currentUser = new CurrentUser(user);
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                         currentUser, null, currentUser.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            } else {
-                System.out.println("User is not authenticated");
             }
         }
         filterChain.doFilter(request, response);

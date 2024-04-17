71ccaa7d904f3cc245e323b64f83591539dd15c6
Özgür Tokay <ozytky@gmail.com>
2024-04-18 01:13:04 +0300

    Update ProfileImage component to handle newImage prop and fallback to defaultProfileImage if src is not provided

--- frontend/src/shared/components/ProfileImage.jsx
+++ frontend/src/shared/components/ProfileImage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import defaultProfileImage from "@/assets/defUser.png";
 
-export const ProfileImage = ({ width, height, className, alt, src }) => {
+export const ProfileImage = ({
+  width,
+  height,
+  className,
+  alt,
+  src,
+  newImage,
+}) => {
   const baseURL = import.meta.env.VITE_APP_WS_BASE_URL;
-  const profileImage = src
-    ? `${baseURL}/assets/profiles/${src}`
-    : defaultProfileImage;
+  const profileImage =
+    newImage || `${baseURL}/assets/profiles/${src}` || defaultProfileImage;
   return (
     <img
       src={profileImage}

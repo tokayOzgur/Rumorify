daf35b34504e4aa98feea1ac9228b47d8ac2b459
Özgür Tokay <ozytky@gmail.com>
2024-04-20 17:45:01 +0300

    Add AccessDeniedException to ErrorHandler for handling access denied errors

--- ws/src/main/java/com/rumorify/ws/exception/ErrorHandler.java
+++ ws/src/main/java/com/rumorify/ws/exception/ErrorHandler.java
@@ -26,7 +26,8 @@ public class ErrorHandler {
             ResourceNotFoundException.class,
             UserNotFoundException.class,
             AuthenticationException.class,
-            FileServiceException.class
+            FileServiceException.class,
+            AccessDeniedException.class
     })
     ResponseEntity<ApiError> handleCustomException(Exception exception, HttpServletRequest request) {
         ApiError error = new ApiError();
@@ -55,6 +56,8 @@ public class ErrorHandler {
             error.setStatus(401);
         } else if (exception instanceof FileServiceException) {
             error.setStatus(500);
+        } else if (exception instanceof AccessDeniedException) {
+            error.setStatus(403);
         }
 
         return ResponseEntity.status(error.getStatus()).body(error);

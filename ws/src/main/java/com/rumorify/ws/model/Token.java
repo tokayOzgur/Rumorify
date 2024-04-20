6f71a37e8a1563c2c58d19f5ed93b6ac4e41a756
Özgür Tokay <ozytky@gmail.com>
2024-04-20 17:46:07 +0300

    Update Token model to include expiration date and isActive flag

--- ws/src/main/java/com/rumorify/ws/model/Token.java
+++ ws/src/main/java/com/rumorify/ws/model/Token.java
@@ -1,30 +1,52 @@
 package com.rumorify.ws.model;
 
+import java.time.OffsetDateTime;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Transient;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
+@Getter
+@Setter
 public class Token {
 
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String token;
 
     @Transient
     private String prefix = "Bearer";
 
+    @Column(nullable = false)
+    private OffsetDateTime expirationDate;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
     @JsonIgnore
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     private User user;
 
-    //TODO: ip address and user location
+    /**
+     * Check if the token is expired
+     * 
+     * @return true if the current date/time is after the expiration date of the
+     *         token
+     */
+    public boolean isExpired() {
+        return OffsetDateTime.now().isAfter(expirationDate);
+    }
+
 }

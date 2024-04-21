abdfe92e9828678e17ed113b82acd8d4c322153a
Özgür Tokay <ozytky@gmail.com>
2024-04-21 22:31:52 +0300

    Add logging

--- ws/src/main/java/com/rumorify/ws/service/impl/UserServiceImpl.java
+++ ws/src/main/java/com/rumorify/ws/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.rumorify.ws.service.impl;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.mail.MailException;
@@ -45,11 +47,17 @@ public class UserServiceImpl implements UserService {
     private EmailService emailService;
     private PasswordEncoder passwordEncoder;
     private final FileService fileService;
+    private static final Logger logger = LogManager.getLogger(UserServiceImpl.class); 
+    
+
 
     @Override
     public GetUserByUserNameResponse findByUsername(String username) {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UserNotFoundException(username));
+                .orElseThrow(() -> {
+                    logger.error("User not found with username: {}", username);
+                    return new UserNotFoundException(username);
+                });
         return mapper.forResponse().map(user, GetUserByUserNameResponse.class);
     }
 
@@ -63,17 +71,19 @@ public class UserServiceImpl implements UserService {
             user.setActive(false);
             userRepository.saveAndFlush(user);
             emailService.sendTokenEmail(user.getEmail(), user.getActivationToken(), 0);
+            logger.debug("User saved successfully: {}", userRequest.getEmail());
         } catch (MailException e) {
+            logger.error("Error sending email: {}, user email: {}", e.getMessage(), userRequest.getEmail(), e);
             throw new ActivationNotificationException();
         }
     }
 
     @Override
     public void activateUser(String token) {
-        User inDB = userRepository.findByActivationToken(token).orElseThrow(() -> new InvalidTokenException());
-        if (inDB == null) {
-            throw new InvalidTokenException();
-        }
+        User inDB = userRepository.findByActivationToken(token).orElseThrow(() -> {
+            logger.error("Invalid user activation token: {}", token);
+            return new InvalidTokenException();
+        });
         inDB.setActive(true);
         inDB.setActivationToken(null);
         userRepository.save(inDB);
@@ -81,34 +91,33 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void updateByUserId(int id, UpdateUserRequest entity) {
-        User inDb = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
-        if (inDb == null)
-            throw new ResourceNotFoundException();
-        if (entity.getFirstName() != null)
-            inDb.setFirstName(entity.getFirstName());
-        if (entity.getLastName() != null)
-            inDb.setLastName(entity.getLastName());
-        if (entity.getUsername() != null)
-            inDb.setUsername(entity.getUsername());
-        if (entity.getProfileDescription() != null)
-            inDb.setProfileDescription(entity.getProfileDescription());
+        User inDb = userRepository.findById(id).orElseThrow(() -> {
+            logger.error("User not found with id: {}", id);
+            return new UserNotFoundException(id);
+        });
+        if (entity.getFirstName() != null) inDb.setFirstName(entity.getFirstName());
+        if (entity.getLastName() != null) inDb.setLastName(entity.getLastName());
+        if (entity.getUsername() != null) inDb.setUsername(entity.getUsername());
+        if (entity.getProfileDescription() != null) inDb.setProfileDescription(entity.getProfileDescription());
         if (entity.getImage() != null && !entity.getImage().equals(inDb.getImage())) {
             fileService.deleteFile(inDb.getImage());
             String fileName = fileService.saveBase4StringAsFile(entity.getImage());
             inDb.setImage(fileName);
         }
         userRepository.save(inDb);
+        logger.debug("User updated successfully: {}", id);
     }
 
     @Override
     public void deleteByUserId(int id) {
-        User inDb = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
-        if (inDb == null)
-            throw new ResourceNotFoundException();
+        User inDb = userRepository.findById(id).orElseThrow(() -> {
+            logger.error("User not found with id: {}", id);
+            return new UserNotFoundException(id);
+        });
         inDb.setDeleted(true);
         fileService.deleteFile(inDb.getImage());
         userRepository.save(inDb);
-
+        logger.debug("User deleted successfully: {}", id);
     }
 
     @Override
@@ -120,27 +129,37 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<GetAllUserResponse> findAll() {
         return userRepository.findAll().stream()
-                .map(user -> this.mapper.forResponse().map(user, GetAllUserResponse.class))
-                .toList();
+                .map(user -> this.mapper.forResponse().map(user, GetAllUserResponse.class)).toList();
     }
 
     @Override
     public GetUserByIdResponse findById(int id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            logger.error("User not found with id: {}", id);
+            return new UserNotFoundException(id);
+        });
         return mapper.forResponse().map(user, GetUserByIdResponse.class);
     }
 
     @Override
     public GetUserByEmailResponse findByEmail(String email) {
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new AuthenticationException());
+        User user = userRepository.findByEmail(email).orElseThrow(() -> {
+            logger.error("User not found with email: {}", email);
+            return new AuthenticationException();
+        });
         return mapper.forResponse().map(user, GetUserByEmailResponse.class);
     }
 
     @Override
     public void resetPassword(PasswordResetRequest passwordResetRequest) {
-        User userInDb = userRepository.findByEmail(passwordResetRequest.getEmail())
-                .orElseThrow(() -> new ResourceNotFoundException());
-        if (userInDb.isDeleted() || !userInDb.isActive()) throw new AccessDeniedException();
+        User userInDb = userRepository.findByEmail(passwordResetRequest.getEmail()).orElseThrow(() -> {
+            logger.error("User not found with email: {}", passwordResetRequest.getEmail());
+            return new ResourceNotFoundException();
+        });
+        if (userInDb.isDeleted() || !userInDb.isActive()) {
+            logger.debug("User is not active or deleted: {}", passwordResetRequest.getEmail());
+            throw new AccessDeniedException();
+        }
         userInDb.setPasswordResetToken(UUID.randomUUID().toString());
         userRepository.save(userInDb);
         emailService.sendTokenEmail(passwordResetRequest.getEmail(), userInDb.getPasswordResetToken(), 1);
@@ -148,12 +167,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void updatePassword(String token, UpdatePasswordRequest request) {
-        User userInDb = userRepository.findByPasswordResetToken(token).orElseThrow(() -> new InvalidTokenException());
-        if (userInDb == null)
-            throw new InvalidTokenException();
+        User userInDb = userRepository.findByPasswordResetToken(token).orElseThrow(() -> {
+            logger.error("Invalid password reset token: {}", token);
+            return new InvalidTokenException();
+        });
         userInDb.setPassword(passwordEncoder.encode(request.getPassword()));
         userInDb.setPasswordResetToken(null);
         userRepository.save(userInDb);
+        logger.debug("Password updated successfully: {}", userInDb.getEmail());
     }
 
 }

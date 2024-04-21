1427600409b8e10fc241264a83700a7be629a552
Özgür Tokay <ozytky@gmail.com>
2024-04-21 21:02:24 +0300

    Fix nullability annotations in StaticResourceConf and TokenFilter

--- ws/src/main/java/com/rumorify/ws/config/StaticResourceConf.java
+++ ws/src/main/java/com/rumorify/ws/config/StaticResourceConf.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,7 +21,7 @@ public class StaticResourceConf implements WebMvcConfigurer {
     private final RumorifyProperties props;
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         String rootPath = Paths.get(props.getStorage().getRoot()).toAbsolutePath().toString();
         registry.addResourceHandler("/assets/**").addResourceLocations("file:" + rootPath + "/")
                 .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
--- ws/src/main/java/com/rumorify/ws/config/TokenFilter.java
+++ ws/src/main/java/com/rumorify/ws/config/TokenFilter.java
@@ -3,6 +3,7 @@ package com.rumorify.ws.config;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,7 +27,7 @@ public class TokenFilter extends OncePerRequestFilter {
     private TokenService tokenService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
         String tokenWithPrefix = getTokenWithPrefix(request);
         if (tokenWithPrefix != null) {

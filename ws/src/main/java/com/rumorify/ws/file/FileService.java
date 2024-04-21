4485b22ba06bb5139e967b1741cb71189dd844be
Özgür Tokay <ozytky@gmail.com>
2024-04-21 22:30:58 +0300

    Add logging to FileService methods

--- ws/src/main/java/com/rumorify/ws/file/FileService.java
+++ ws/src/main/java/com/rumorify/ws/file/FileService.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.UUID;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,7 @@ public class FileService {
     @Autowired
     private RumorifyProperties props;
     private Tika tika = new Tika();
+    private static final Logger logger = LogManager.getLogger(FileService.class);
 
     public String saveBase4StringAsFile(String image) {
         String fileName = UUID.randomUUID().toString() + "." + getFileType(image).split("/")[1];
@@ -30,8 +33,10 @@ public class FileService {
             OutputStream os = new FileOutputStream(path.toFile());
             os.write(decodedImage(image));
             os.close();
+            logger.info("File saved: " + fileName);
             return fileName;
         } catch (IOException e) {
+            logger.error("Error saving file: " + fileName);
             throw new FileServiceException("rumorify.error.file.save"); // TODO: test et
         }
     }
@@ -45,12 +50,16 @@ public class FileService {
     }
 
     public void deleteFile(String image) {
-        if (image == null || image.isEmpty())
+        if (image == null || image.isEmpty()) {
+            logger.warn("File name is null or empty");
             return;
+        }
         Path path = getFilePath(image);
         try {
             Files.deleteIfExists(path);
+            logger.info("File deleted: " + image);
         } catch (IOException e) {
+            logger.error("Error deleting file: " + image);
             throw new FileServiceException("rumorify.error.file.delete"); // TODO: test et
         }
     }

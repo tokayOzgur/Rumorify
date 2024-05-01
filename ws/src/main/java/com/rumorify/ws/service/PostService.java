337da6b2955cf8fad64cebdc446ac7071a6391d8
Özgür Tokay <ozytky@gmail.com>
2024-05-02 01:10:47 +0300

    Refactor PostService interface and add new methods for updating and deleting posts

--- ws/src/main/java/com/rumorify/ws/service/PostService.java
+++ ws/src/main/java/com/rumorify/ws/service/PostService.java
@@ -1,9 +1,29 @@
 package com.rumorify.ws.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
 import com.rumorify.ws.dto.requests.CreatePostRequest;
+import com.rumorify.ws.dto.requests.UpdatePostRequest;
+import com.rumorify.ws.dto.responses.GetAllPostResponse;
+import com.rumorify.ws.dto.responses.GetPostByIdResponse;
 
 public interface PostService {
-        
-        public void save(CreatePostRequest post, int id);
-        
+
+        public void save(CreatePostRequest post, int currentUserId);
+
+        public void updateByPostId(Long id, UpdatePostRequest entity);
+
+        public void deleteByPostId(Long id);
+
+        public List<GetAllPostResponse> findAll(Pageable pageable);
+
+        public GetPostByIdResponse findById(Long id);
+
+        public List<GetAllPostResponse> findByUserId(Pageable pageable, int userId);
+
+        public List<GetAllPostResponse> findByUserIds(Pageable pageable, List<Integer> userIds);
+
+        public List<GetAllPostResponse> findByIsDeletedFalse(Pageable pageable);
 }

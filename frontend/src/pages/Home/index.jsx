f4b46513c9da41d46c446b307ecf7fc95287cc28
Özgür Tokay <ozytky@gmail.com>
2024-05-05 17:07:04 +0300

    feat: Add PostList component to Home page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,13 +1,16 @@
 import { UserList } from "@/pages/Home/components/UserList";
+import { PostList } from "./components/PostList";
 
 export const Home = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6 col-sm-12">
+        <div className="col-md-5 col-sm-12">
           <UserList />
         </div>
-        <div className="col-md-6 col-sm-12"></div>
+        <div className="col-md-7 col-sm-12">
+          <PostList />
+        </div>
       </div>
     </div>
   );

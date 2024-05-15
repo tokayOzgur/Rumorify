e806284e13719826d78c204513bb9a2768bc239b
Özgür Tokay <ozytky@gmail.com>
2024-05-16 00:02:40 +0300

    Refactor PostMedia component to handle different media types

--- frontend/src/shared/components/PostMedia.jsx
+++ frontend/src/shared/components/PostMedia.jsx
@@ -11,8 +11,25 @@ export const PostMedia = ({
   const baseURL = import.meta.env.VITE_APP_WS_BASE_URL;
   const postMedia = base64src || `${baseURL}/assets/posts/${src}`;
 
-  const mediaType = postMedia.split(".")[1];
-  return mediaType !== "mp4" ? (
+  const isBase64 = postMedia.startsWith("data:");
+  let mediaType;
+
+  if (isBase64) {
+    const mimeType = postMedia.split(";")[0].split(":")[1];
+    mediaType = mimeType.split("/")[0];
+  } else {
+    const fileExtension = postMedia.split(".").pop();
+    const imageExtensions = ["jpg", "jpeg", "png", "gif"];
+    const videoExtensions = ["mp4", "mov", "avi", "webm"];
+
+    if (imageExtensions.includes(fileExtension.toLowerCase())) {
+      mediaType = "image";
+    } else if (videoExtensions.includes(fileExtension.toLowerCase())) {
+      mediaType = "video";
+    }
+  }
+
+  return mediaType === "image" ? (
     <img
       src={postMedia}
       alt={alt}

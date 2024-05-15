61330b692dcb4aa3495206742d462a078784ac3c
Özgür Tokay <ozytky@gmail.com>
2024-05-16 00:02:05 +0300

    Refactor PostListItem component to display post media

--- frontend/src/shared/components/PostListItem.jsx
+++ frontend/src/shared/components/PostListItem.jsx
@@ -1,7 +1,6 @@
 import { PostMedia } from "./PostMedia";
 
 export const PostListItem = ({ post }) => {
-
   const formatDate = (timestamp) => {
     const date = new Date(timestamp);
     return new Intl.DateTimeFormat("tr-TR", {
@@ -13,36 +12,38 @@ export const PostListItem = ({ post }) => {
       hour12: false,
     }).format(date);
   };
-  console.log("post::", post)
+  console.log("post::", post);
   return (
     <div className="border rounded mb-1">
       <div className="row p-1">
-        <div className="col-6">
+        <div className="col-5">
           <b>{post.username}</b>
           <p>{post.content}</p>
           <small>{formatDate(post.creationTime)}</small>
         </div>
-        <div className="col-6">
-          {post.imageUrl && (
-            <div className={post.videoUrl ? "col-6" : "col-12"}>
-              <PostMedia
-                className="img-thumbnail"
-                alt={"postImage"}
-                width={"100%"}
-                src={post.imageUrl}
-              />
-            </div>
-          )}
-          {post.videoUrl && (
-            <div className={post.imageUrl ? "col-6" : "col-12"}>
-              <PostMedia
-                className="img-thumbnail"
-                alt={"postVideo"}
-                width={"100%"}
-                src={post.videoUrl}
-              />
-            </div>
-          )}
+        <div className="col-7">
+          <div className="row">
+            {post.imageUrl && (
+              <div className={post.videoUrl ? "col-6" : "col-12"}>
+                <PostMedia
+                  className="img-thumbnail"
+                  alt={"postImage"}
+                  width={"100%"}
+                  src={post.imageUrl}
+                />
+              </div>
+            )}
+            {post.videoUrl && (
+              <div className={post.imageUrl ? "col-6" : "col-12"}>
+                <PostMedia
+                  className="img-thumbnail"
+                  alt={"postVideo"}
+                  width={"100%"}
+                  src={post.videoUrl}
+                />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
